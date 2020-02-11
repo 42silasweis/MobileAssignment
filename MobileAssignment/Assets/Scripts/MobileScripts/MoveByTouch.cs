1d99c296ca8c454f94fe503237863516db8b19ad
Silas Weis <18.silas.weis@ksd.org>
2020-02-11 13:29:47 -0800

    Added pathfinding files and test enemy

--- MobileAssignment/Assets/Scripts/MobileScripts/MoveByTouch.cs
+++ MobileAssignment/Assets/Scripts/MobileScripts/MoveByTouch.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class MoveByTouch : MonoBehaviour
 {
-
+    public Transform camera;
 
     void Update()
     {
         for (int i = 0; i < Input.touchCount; i++)
         {
             Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
-            Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
+            Debug.DrawLine(camera.transform.position, touchPosition, Color.red);
+            Debug.Log("Touching screen");
         }
     }
 }
