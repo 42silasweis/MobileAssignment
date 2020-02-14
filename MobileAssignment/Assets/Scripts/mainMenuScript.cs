277f6c99203ecd3daf74f88869c90ab03e2c9dca
Dalton Haer <20.dalton.haer@ksd.org>
2020-02-14 13:51:22 -0800

    added more UI elements, created the main menu scene and started setting up the pause button that the player will use within a level.

--- MobileAssignment/Assets/Scripts/mainMenuScript.cs
+++ MobileAssignment/Assets/Scripts/mainMenuScript.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainMenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        GetComponent<Canvas>().enabled = false;
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
