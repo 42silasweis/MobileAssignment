fdf7ebb7c0ce7d3a9cf1803ada69236371258071
Silas Weis <18.silas.weis@ksd.org>
2020-02-19 13:30:15 -0800

    Added additional raycasts the EnemyTest2

--- MobileAssignment/Assets/EnemyDetectionScript.cs
+++ MobileAssignment/Assets/EnemyDetectionScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyDetectionScript : MonoBehaviour
 {
     public float distance;
+    public bool playerIsInSight = false;
 
     private void Start()
     {
@@ -12,24 +13,229 @@ public class EnemyDetectionScript : MonoBehaviour
     }
     void Update()
     {
+        // Numbering starts from top to bottom meaning top starts at 1
 
-        Vector3 noAngle = transform.forward;
-        Quaternion spreadAngle = Quaternion.AngleAxis(-15, new Vector3(0, 1, 0));
+        Vector3 noAngle = transform.right; //This and the next two lines is for Raycast 1 offset at an angle of -15 from the transform.right position
+        Quaternion spreadAngle = Quaternion.AngleAxis(10, new Vector3(0, 0, 1));
         Vector3 newVector = spreadAngle * noAngle;
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
-        if(hitInfo.collider != null)
+
+        Quaternion spreadAngle2 = Quaternion.AngleAxis(350, new Vector3(0, 0, 1)); //This and the one below it is for Raycast 3 offset at an angle of 15 from the transform.right position
+        Vector3 newVector2 = spreadAngle2 * noAngle;
+
+        Quaternion spreadAngle3 = Quaternion.AngleAxis(20, new Vector3(0, 0, 1)); //This and the one below it is for Raycast 4 offset at an angle of 15 from the transform.right position
+        Vector3 newVector3 = spreadAngle3 * noAngle;
+
+        Quaternion spreadAngle4 = Quaternion.AngleAxis(340, new Vector3(0, 0, 1)); //This and the one below it is for Raycast 5 offset at an angle of 15 from the transform.right position
+        Vector3 newVector4 = spreadAngle4 * noAngle;
+
+
+        RaycastHit2D hitInfo1 = Physics2D.Raycast(transform.position, newVector, distance);
+        RaycastHit2D hitInfo2 = Physics2D.Raycast(transform.position, transform.right, distance); // This is the center Raycast
+        RaycastHit2D hitInfo3 = Physics2D.Raycast(transform.position, newVector2, distance);
+        RaycastHit2D hitInfo4 = Physics2D.Raycast(transform.position, newVector3, distance);
+        RaycastHit2D hitInfo5 = Physics2D.Raycast(transform.position, newVector4, distance);
+
+
+        if (hitInfo1.collider != null)
         {
-            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            Debug.DrawLine(transform.position, hitInfo1.point, Color.red);
             //Debug.Log("Hit Collider");
-            if (hitInfo.collider.CompareTag("Player"))
+            if (hitInfo1.collider.CompareTag("Player"))
             {
-                Debug.Log("Ray hit the Player Collider");
+                //Debug.Log("Ray hit the Player Collider");
+                if (!playerIsInSight)
+                {
+                    playerIsInSight = true;
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + newVector * distance, Color.green);
+        }
+
+
+        if (hitInfo2.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo2.point, Color.red);
+            //Debug.Log("Hit Collider");
+            if (hitInfo2.collider.CompareTag("Player"))
+            {
+                //Debug.Log("Ray hit the Player Collider");
+                if (!playerIsInSight)
+                {
+                    playerIsInSight = true;
+                }
             }
         }
         else
         {
             Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
-           // Debug.Log("Is not hitting a Collider");
         }
+
+
+        if (hitInfo3.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo3.point, Color.red);
+            //Debug.Log("Hit Collider");
+            if (hitInfo3.collider.CompareTag("Player"))
+            {
+                //Debug.Log("Ray hit the Player Collider");
+                if (!playerIsInSight)
+                {
+                    playerIsInSight = true;
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + newVector2 * distance, Color.green);
+        }
+
+        if (hitInfo4.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo4.point, Color.red);
+            //Debug.Log("Hit Collider");
+            if (hitInfo4.collider.CompareTag("Player"))
+            {
+                //Debug.Log("Ray hit the Player Collider");
+                if (!playerIsInSight)
+                {
+                    playerIsInSight = true;
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + newVector3 * distance, Color.green);
+        }
+
+        if (hitInfo5.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo5.point, Color.red);
+            //Debug.Log("Hit Collider");
+            if (hitInfo5.collider.CompareTag("Player"))
+            {
+                //Debug.Log("Ray hit the Player Collider");
+                if (!playerIsInSight)
+                {
+                    playerIsInSight = true;
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + newVector4 * distance, Color.green);
+        }
+
+
+
+        if (playerIsInSight && hitInfo1.collider == null && hitInfo2.collider == null && hitInfo3.collider == null)
+        {
+             playerIsInSight = false;
+        }
+
+
+        /*
+        if (hitInfoTop.collider != null || hitInfoMiddle.collider != null || hitInfoBottom.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfoTop.point, Color.red);
+            Debug.DrawLine(transform.position, hitInfoMiddle.point, Color.red);
+            Debug.DrawLine(transform.position, hitInfoBottom.point, Color.red);
+
+            if (hitInfoTop.collider.CompareTag("Player") || hitInfoMiddle.collider.CompareTag("Player") || hitInfoBottom.collider.CompareTag("Player"))
+            {
+                Debug.Log("Ray hit the Player Collider");
+                if (!playerIsInSight)
+                {
+                    playerIsInSight = true;
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + newVector * distance, Color.green);
+            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+            Debug.DrawLine(transform.position, transform.position + newVector2 * distance, Color.green);
+
+            if (playerIsInSight)
+            {
+                playerIsInSight = false;
+            }
+        }
+        
+
+
+        if (hitInfoTop.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfoTop.point, Color.red);
+            //Debug.Log("Hit Collider");
+            if (hitInfoTop.collider.CompareTag("Player"))
+            {
+                Debug.Log("Ray hit the Player Collider");
+                if (!playerIsInSight)
+                {
+                    playerIsInSight = true;
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + newVector * distance, Color.green);
+            // Debug.Log("Is not hitting a Collider");
+            if (playerIsInSight && hitInfoMiddle.collider == null || hitInfoBottom.collider == null)
+            {
+                playerIsInSight = false;
+            }
+        }
+        
+
+        if (hitInfoMiddle.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfoMiddle.point, Color.red);
+            //Debug.Log("Hit Collider");
+            if (hitInfoMiddle.collider.CompareTag("Player"))
+            {
+                Debug.Log("Ray hit the Player Collider");
+                if (!playerIsInSight)
+                {
+                    playerIsInSight = true;
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+            // Debug.Log("Is not hitting a Collider");
+            if (playerIsInSight && hitInfoTop.collider == null || hitInfoBottom.collider == null)
+            {
+                playerIsInSight = false;
+            }
+        }
+        
+
+        if (hitInfoBottom.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfoBottom.point, Color.red);
+            //Debug.Log("Hit Collider");
+            if (hitInfoBottom.collider.CompareTag("Player"))
+            {
+                Debug.Log("Ray hit the Player Collider");
+                if (!playerIsInSight)
+                {
+                    playerIsInSight = true;
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + newVector2 * distance, Color.green);
+            // Debug.Log("Is not hitting a Collider");
+            if (playerIsInSight && hitInfoTop.collider == null || hitInfoMiddle.collider == null)
+            {
+                playerIsInSight = false;
+            }
+        }
+        */
     }
 }
