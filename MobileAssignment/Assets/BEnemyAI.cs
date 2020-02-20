4644a518d428fad11fe7de1e33fbf4c1a8fcfde2
Silas Weis <18.silas.weis@ksd.org>
2020-02-20 13:58:37 -0800

    Created enemy patrols and player detection during patrol

--- MobileAssignment/Assets/BEnemyAI.cs
+++ MobileAssignment/Assets/BEnemyAI.cs
@@ -6,8 +6,17 @@ using Pathfinding;
 public class BEnemyAI : MonoBehaviour
 {
     public float stopDistance = 0.35f;
+
     public Transform target;
+
     public float speed = 4f;
+    public float slowSpeed = 2f;
+    public float noiseLevel;
+
+
+    public float noiseCauseSuspicionLevel = 1.0f;
+    public bool enemySeesPlayer;
+
     public float nextWayPointDistance = 1f;
     Path path;
     int currentWaypoint = 0;
@@ -15,7 +24,11 @@ public class BEnemyAI : MonoBehaviour
     Seeker seeker;
     Rigidbody2D rb;
 
+    public float heardNoiseFollowDuration = 3f;
+    float heardNoiseTimer = 10f;
     public bool runToPlayer;
+
+    public bool canPatrol;
 
     // Start is called before the first frame update
     void Start()
@@ -43,6 +56,21 @@ public class BEnemyAI : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        heardNoiseTimer += Time.deltaTime;
+        
+        enemySeesPlayer = GameObject.FindObjectOfType<EnemyDetectionScript>().playerIsInSight;
+
+
+        if(enemySeesPlayer && canPatrol || canPatrol && noiseLevel > noiseCauseSuspicionLevel)
+        {
+            canPatrol = false;
+        }
+        else if (!canPatrol && heardNoiseTimer > heardNoiseFollowDuration)
+        {
+            canPatrol = true;
+        }
+
+
         if (path == null)
         {
             return;
@@ -58,10 +86,16 @@ public class BEnemyAI : MonoBehaviour
         }
         Vector3 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
         Vector2 force = direction * speed;// * Time.deltaTime;
+        Vector2 forceSlow = direction * slowSpeed;// * Time.deltaTime;
         direction.z = transform.position.z;
         //rb.AddForce(force);
         Vector2 moveDir = new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y);
-        if (moveDir.magnitude > stopDistance && runToPlayer)
+
+        if (moveDir.magnitude < 6)
+        {
+            noiseLevel = GameObject.FindObjectOfType<PlayerMovementScript>().suspicion;
+        }
+        if (moveDir.magnitude > stopDistance && enemySeesPlayer)
         {
             //rb.velocity = force;
             rb.AddForce(force);
@@ -72,7 +106,16 @@ public class BEnemyAI : MonoBehaviour
             rb.velocity = new Vector2 (0, 0); 
             //Debug.Log("Is not supposed to move");
         }
-        
+
+        if(noiseLevel >= noiseCauseSuspicionLevel && !enemySeesPlayer)
+        {
+            heardNoiseTimer = 0;
+        }
+
+        if(heardNoiseTimer < heardNoiseFollowDuration && !enemySeesPlayer)
+        {
+            rb.AddForce(forceSlow);
+        }
 
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
 
---
+++ MobileAssignment/Assets/EnemyPatrolScript.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyPatrolScript : MonoBehaviour
+{
+    public float stopDistance = 0.35f;
+    float moveDirDist;
+    public Transform player;
+    public Transform target;
+    public Transform patrolLocation1;
+    public Transform patrolLocation2;
+    public Transform patrolLocation3;
+    public Transform patrolLocation4;
+    public Transform patrolLocation5;
+
+    float randomSelectedLocation;
+
+    public float speed = 4f;
+    float noiseLevel;
+
+    public float noiseCauseSuspicionLevel = 1.0f;
+    bool enemySeesPlayer;
+
+    public float nextWayPointDistance = 1f;
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    public float minWaitForThisLong = 3;
+    public float maxWaitForThisLong = 6;
+    public float randomWaitTime;
+    float waitTimer;
+    bool canPatrol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+
+        randomWaitTime = Random.Range(minWaitForThisLong, maxWaitForThisLong);
+        randomSelectedLocation = Random.Range(1, 5);
+    }
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        waitTimer += Time.deltaTime;
+        noiseLevel = GameObject.FindObjectOfType<BEnemyAI>().noiseLevel;
+        canPatrol = GameObject.FindObjectOfType<BEnemyAI>().canPatrol;
+
+        switch (randomSelectedLocation)
+        {
+            case 1:
+                target = patrolLocation1;
+                break;
+            case 2:
+                target = patrolLocation2;
+                break;
+            case 3:
+                target = patrolLocation3;
+                break;
+            case 4:
+                target = patrolLocation4;
+                break;
+            case 5:
+                target = patrolLocation5;
+                break;
+        }
+
+        if(rb.velocity.magnitude > 0.2f)
+        {
+            waitTimer = 0;
+        }
+
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+        Vector3 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed;// * Time.deltaTime;
+        direction.z = transform.position.z;
+        //rb.AddForce(force);
+        Vector2 moveDir = new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y);
+
+        moveDirDist = moveDir.magnitude;
+
+        if (canPatrol)
+        {
+            if (moveDir.magnitude >= stopDistance)
+            {
+                //rb.velocity = force;
+                rb.AddForce(force);
+            }
+
+            if (moveDir.magnitude <= 0.9f && rb.velocity.magnitude <= 0.3f)
+            {
+                if (waitTimer >= randomWaitTime)
+                {
+                    randomWaitTime = Random.Range(minWaitForThisLong, maxWaitForThisLong);
+                    randomSelectedLocation = Random.Range(1, 5);
+                }
+                //Debug.Log("Is not supposed to move");
+            }
+
+            float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+            if (distance < nextWayPointDistance)
+            {
+                currentWaypoint++;
+            }
+        }
+        
+    }
+}
