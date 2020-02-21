76b07d481acde72f2a85e462d2733fbbc67f2496
Dalton Haer <20.dalton.haer@ksd.org>
2020-02-21 14:00:49 -0800

    adding sprite for main character.

--- MobileAssignment/Assets/Scripts/GalaxiasScripts/OptionsMenu.cs
+++ MobileAssignment/Assets/Scripts/GalaxiasScripts/OptionsMenu.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public bool optionStatus = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Canvas>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (optionStatus == true)
+            {
+                GetComponent<Canvas>().enabled = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                GetComponent<Canvas>().enabled = false;
+                Time.timeScale = 1;
+            }
+        }
     }
 }
